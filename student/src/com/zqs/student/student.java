596379019f1d6c93dff717ccc5f8cc2e2f02bc56
Zqingshan <z_qingshan@163.com>
2019-05-16 09:09:01 +0800

    初始化工程

---
+++ student/src/com/zqs/student/student.java
@@ -0,0 +1,47 @@
+package com.zqs.student;
+
+public class student {
+	private int id;
+	private String username;
+	private String password;
+	private int age;
+	private int sex;
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getUsername() {
+		return username;
+	}
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public int getAge() {
+		return age;
+	}
+	public void setAge(int age) {
+		this.age = age;
+	}
+	public int getSex() {
+		return sex;
+	}
+	public void setSex(int sex) {
+		this.sex = sex;
+	}
+	@Override
+	public String toString() {
+		return "student [id=" + id + ", username=" + username + ", password=" + password + ", age=" + age + ", sex="
+				+ sex + "]";
+	}
+	
+	
+}
